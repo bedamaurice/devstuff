99df457e22b6a3837dca24ee19453f09b90e1ff8
bedamaurice <bedaarnold@gmail.com>
2025-02-12 17:48:45 +0100

    neovim config

--- config_zsh.sh
+++ config_zsh.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
+#variables
+ZSHRC_CONFIG=$HOME/.zshrc
 
 echo "Do you want the zsh as default shell? (y=yes)"
 read dshell
 
 if [ $dshell = "y" ]; then 
     echo "zsh will your default shell"
-    sudo chsh -s "$(which zsh)" "$USER"
+    sudo chsh -s "$(which zsh)" $USER
     if [ $? -eq 0 ]; then
         echo "The default shell has been changed to zsh. Please log out and log back in for the changes to take effect."
     else
@@ -14,11 +16,12 @@ if [ $dshell = "y" ]; then
     echo "You want change the .zshrc config? (y=yes)"
     read ch_zshrc
     if [ $ch_zshrc = "y" ]; then
-        mv $HOME/.zshrc $HOME/.zshrc.bak
+        mv $ZSHRC_CONFIG $ZSHRC_CONFIG.bak
         cp ./zsh/.zshrc $HOME/.zshrc
-        if [ -f $HOME/.zshrc ];then
+        mkdir -p $HOME/.zsh
+        if [ -f $ZSHRC_CONFIG ];then
             echo ".zshrc is now in your home folder"
-            echo "## Plugins are here:" >> $Home/.zshrc
+            echo "## Plugins are here:" >> $ZSHRC_CONFIG
         else
             echo "no .zshrc was found in your homefolder"
         fi
@@ -29,28 +32,6 @@ if [ $dshell = "y" ]; then
         mkdir -p ~/.zsh
         cp ./zsh/zsh-alias/zsh-alias.zsh ~/.zsh/zsh-alias/zsh-alias.zsh
         echo source $HOME/.zsh/zsh-alias/zsh-alias.zsh >> ~/.zshrc
-    fi
-    echo "You want plugins like zsh-autosuggestions and syntax-higlighting?"
-    read zplugin
-    if [ $zplugin = "y" ]; then
-        mkdir -p ~/.zsh
-        git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions
-        echo source $HOME/.zsh/zsh-autosuggestions/zsh-autosuggestions.zsh >> ~/.zshrc
-        P_auto='~/.zsh/zsh-autosuggestions/zsh-autosuggestions.zsh'
-        if [ -f $P_auto ]; then
-            echo "zsh-autosugesstions was installed successful"
-        else
-            echo "File zsh-autosugesstion.zsh was not found"
-        fi
-        exec git clone https://github.com/zsh-users/zsh-syntax-highlighting ~/.zsh/zsh-syntax-highlighting
-        echo source $HOME/.zsh/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh >> ~/.zshrc
-        P_high='~/.zsh/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh'
-        if [ -f $P_auto ]; then
-            echo "zsh-highlighting was installed successful"
-        else
-            echo "File zsh-highlighting.zsh not found"
-        fi
-        echo "##Plugins ends here" >> $HOME/.zshrc
     fi
 fi
 
